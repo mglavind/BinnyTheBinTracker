f33f0be9750351a653ede8e1b00ff3a274a5b129
Markus Glavind <glavindmarkus@gmail.com>
2023-04-13 09:50:25 +0200

    Nu med Dummy code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,90 @@
 #include <Arduino.h>
 
+
+// Begin setup for Wireless communitcation
+#define DummyVariable
+// End setup for Wireless communitcation
+
+
+// Begin setup for Sensors
+
+// Husk at tilføje libraries gennem Platform.io menuen, for så installerer den det hos os alle
+
+// End setup for Sensors
+
+
+// Begin setup for 
+
+// End setup for 
+
+
+// Begin setup for 
+
+// End setup for 
+
+
+  //////////////////////////////////////
+ ///////* Everything Sensor *//////////
+//////////////////////////////////////
+
+void SensorInitialization() {
+  // wite stuff to initialize the sensor here
+}
+
+void ReadSensor1() {
+  // wite stuff to initialize the sensor here
+}
+
+void ReadSensor2() {
+  // wite stuff to initialize the sensor here
+}
+
+void ReadSensor3() {
+  // wite stuff to initialize the sensor here
+}
+
+
+  ////////////////////////////////////////
+ ///////* Everything Wireless *//////////
+////////////////////////////////////////
+
+void WirelessInitialization() {
+  // wite stuff to initialize the wireless communication here
+}
+
+void WirelessTX() {
+  // wite stuff to initialize the wireless communication here
+}
+void WirelessRX() {
+  // wite stuff to initialize the wireless communication here
+}
+
+
+  /////////////////////////////////////////////
+ ///////* Everything Låsemekanisme *//////////
+/////////////////////////////////////////////
+
+void LockInitialization() {
+  // wite stuff to initialize the wireless communication here
+}
+
+void LockA() {
+  // wite stuff to initialize the wireless communication here
+}
+void LockB() {
+  // wite stuff to initialize the wireless communication here
+}
+
+
+
+
+
+
+
+  ///////////////////////////////////
+ ///////* The main code *///////////
+///////////////////////////////////
+
 void setup() {
   // put your setup code here, to run once:
 }
@@ -7,3 +92,5 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+
