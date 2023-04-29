0b7ce6fe0c01361c7694ac170d97750ef27cd54a
Markus Glavind <glavindmarkus@gmail.com>
2023-04-29 19:18:27 +0200

    Added Sleep mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,82 @@
 #include <Arduino.h>
 
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+
+long timeToSleep = 1000*60; // Timer interval in milliseconds
+
+
+
+
+
+
+  //////////////////////////////////////
+ ///////////* Deep sleep */////////////
+//////////////////////////////////////
+
+
+
+//   https://randomnerdtutorials.com/esp32-deep-sleep-arduino-ide-wake-up-sources/
+// Lav evt en interrupt også
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
+bool GoToSleep = false;
+
+
+/*
+Method to print the reason by which ESP32
+has been awaken from sleep
+*/
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
+
+void SetupSleepMode() {
+    //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  /*
+  First we configure the wake up source
+  We set our ESP32 to wake up every 5 seconds
+  */
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+}
+
+  //////////////////////////////////////
+ ///////////* Deep sleep */////////////
+//////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
   //////////////////////////////////////
  /////////////* EEPROM *///////////////
 //////////////////////////////////////
@@ -447,10 +524,14 @@ void setup() {
   
   //////////////////////////////////////////////////////////////////////////////////////////// Wireless
   LoraSetup();
+  
+  //////////////////////////////////////////////////////////////////////////////////////////// DeepSleep
+  SetupSleepMode();
 
 }
 
 void loop() {
+currentMillis = millis();
 
 //////////////////////////////////////////////////////////////////////////////////////////// RFID
 // Her skal låsen aktiveres, hvis kortet er rigtigt
@@ -498,6 +579,21 @@ if (SendFillLevel) {
   //////////////////////////////////////////////////////////////////////////////////////////// EEPROM
   // Her gemmer vi vores KnownUIDs til EEPROM
   SaveUIDs(); 
+
+if (currentMillis - previousMillis >= timeToSleep) {
+  GoToSleep = true; // This way, GoToSleep can be triggered other places too.
+}
+
+if(GoToSleep) {
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed"); 
+}
+  
+
+
 
 }
 
